ed3d51466f3446136ce76b524d1208259f75453c
ammarbahtiarasli <ammarbahtiarasli@gmail.com>
2022-02-26 21:51:56 +0700

    menyelesaikan pertemuan 3

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,15 @@
+<?php
+// pengkondisian / pencabangan
+// if else
+// if elseif else
+// ternary operator
+// switch
+
+$x = 20;
+if ($x < 20) {
+    echo "benar";
+} elseif ($x == 20) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
